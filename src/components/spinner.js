4e32ec975bc1f94895ae2bbf5dd04d2ab58487c8
Tony D'Addeo <tony@askhr.com>
2020-04-11 23:33:25 -0500

    Add spinner

---
+++ src/components/spinner.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../loader.css"
+
+export const Spinner = () => (
+  <div className="h-full w-full flex items-center justify-center">
+    <div className="ball-triangle-path">
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+)
