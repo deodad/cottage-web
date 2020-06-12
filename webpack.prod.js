7afd3fab729e2e6735c4ec70f1753b74d2198694
Tony D'Addeo <tony@askhr.com>
2020-06-11 22:28:05 -0500

    Load env vars in prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: "production",
   plugins: [
+    new webpack.EnvironmentPlugin(['NODE_ENV', 'COTTAGE_API_HOST']),
     new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime/, /main/]),
   ],
 })
