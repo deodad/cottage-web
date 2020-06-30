5e5dbe71af25eacc3dd5a43ec1d2984ed5e9afbe
Tony D'Addeo <tony@askhr.com>
2020-06-30 11:31:10 -0500

    Add missing add-listing file

---
+++ src/roots/add-listing.js
@@ -0,0 +1,6 @@
+import { lazy } from "react"
+import { withUserPage } from "../hoc"
+
+const AddListing = lazy(() => import("../pages/add-listing"))
+
+export default withUserPage({ page: { title: "Add listing", back: true }})(AddListing)
