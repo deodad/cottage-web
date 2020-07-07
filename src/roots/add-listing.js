580a4e78dd409540dd11d943b66d674d946f7144
Tony D'Addeo <tony@askhr.com>
2020-07-06 22:19:32 -0500

    Focus during add listing

--- src/roots/add-listing.js
+++ src/roots/add-listing.js
@@ -3,4 +3,12 @@ import { withUserPage } from "../hoc"
 
 const AddListing = lazy(() => import("../pages/add-listing"))
 
-export default withUserPage({ page: { title: "Add listing", back: true }})(AddListing)
+export default withUserPage({ 
+  page: { 
+    title: "Add listing", 
+    back: true 
+  },
+  layout: {
+    focus: true
+  }
+})(AddListing)
